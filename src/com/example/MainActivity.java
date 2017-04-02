f86a9dc0385976636c095dc94e530c102991cc2e
Nishit Prasad <nshtpd@gmail.com>
2017-04-02 18:26:32 -0400

    Update MainActivity.java

--- src/com/example/MainActivity.java
+++ src/com/example/MainActivity.java
@@ -16,71 +16,60 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
-		gestureDetector = new GestureDetector(new SwipeGestureDetector());
-		
+		gestureDetector = new GestureDetector(new SwipeGestureDetector());		
 	}
-	 @Override
-	  public boolean onTouchEvent(MotionEvent event) {
-	    if (gestureDetector.onTouchEvent(event)) {
-	      return true;
-	    }
-	    return super.onTouchEvent(event);
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		if (gestureDetector.onTouchEvent(event)) {
+			return true;
+		}
+		return super.onTouchEvent(event);
 	  }
 
 	  private void onLeftSwipe() {
 		  Intent i = new Intent(getApplicationContext(),Left.class);
 		  startActivity(i);
 		  finish();
-	    // Do something
 	  }
 
 	  private void onRightSwipe() {
 		  Intent i = new Intent(getApplicationContext(),Right.class);
 		  startActivity(i);
 		  finish();
-	    // Do something
 	  }
 
-	  // Private class for gestures
-	  private class SwipeGestureDetector
-	          extends SimpleOnGestureListener {
+	  /* Private class for gestures */
+	  private class SwipeGestureDetector extends SimpleOnGestureListener {
 	    // Swipe properties, you can change it to make the swipe
 	    // longer or shorter and speed
-	    private static final int SWIPE_MIN_DISTANCE = 120;
-	    private static final int SWIPE_MAX_OFF_PATH = 200;
-	    private static final int SWIPE_THRESHOLD_VELOCITY = 200;
-
-	    public boolean onFling(MotionEvent e1, MotionEvent e2,
-	                         float velocityX, float velocityY) {
-	      try {
-	        float diffAbs = Math.abs(e1.getY() - e2.getY());
-	        float diff = e1.getX() - e2.getX();
-
-	        if (diffAbs > SWIPE_MAX_OFF_PATH)
-	          return false;
-	       
-	        // Left swipe
-	        if (diff > SWIPE_MIN_DISTANCE
-	        && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
-	           MainActivity.this.onLeftSwipe();
-
-	        // Right swipe
-	        } else if (-diff > SWIPE_MIN_DISTANCE
-	        && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
-	        	 MainActivity.this.onRightSwipe();
-	        }
-	      } catch (Exception e) {
-	        Log.e("YourActivity", "Error on gestures");
-	      }
-	      return false;
-	    }
+		  private static final int SWIPE_MIN_DISTANCE = 120;
+		  private static final int SWIPE_MAX_OFF_PATH = 200;
+		  private static final int SWIPE_THRESHOLD_VELOCITY = 200;
+		  
+		  public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+			  try {
+				  float diffAbs = Math.abs(e1.getY() - e2.getY());
+				  float diff = e1.getX() - e2.getX();
+				  
+				  if (diffAbs > SWIPE_MAX_OFF_PATH)
+					  return false;
+				  
+				  if (diff > SWIPE_MIN_DISTANCE && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+					  MainActivity.this.onLeftSwipe();
+				  } else if (-diff > SWIPE_MIN_DISTANCE && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+					  MainActivity.this.onRightSwipe();
+				  }
+			  } catch (Exception e) {
+				  Log.e("YourActivity", "Error on gestures");
+			  }
+			  return false;
+		  }
 	  }
 	
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
+		/* Inflate the menu; this adds items to the action bar if it is present. */
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
